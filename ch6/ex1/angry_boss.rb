f58209b7bfebb9d68f9d23a2619eb73e889bf50a
Richard van der Dys <rhvanderdys@containerstore.com>
2013-07-16 13:12:54 -0500

    stuff

---
+++ ch6/ex1/angry_boss.rb
@@ -0,0 +1,3 @@
+puts "What do you want?!?"
+str = gets().chomp
+puts "WHADDAYA MEAN \"" + str.capitalize() + "\"?!? YOU'RE FIRED!!"
---
+++ ch7/ex1/99_bottles.rb
@@ -0,0 +1,9 @@
+#99 bottles of beer on the wall, 99 bottles of beer.
+#Take one down and pass it around, 98 bottles of beer on the wall.
+
+i = 3
+
+while i > 1 do
+	puts i.to_s() + " bottles of beer on the wall, " + i.to_s() + " bottles of beer.
+	puts Take one down and pass it around, 98 bottles of beer on the wall.
+end
