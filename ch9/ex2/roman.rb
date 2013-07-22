277127e0151b1c6d127a9df0d44e99c5c790ebaa
Richard van der Dys <rhvanderdys@containerstore.com>
2013-07-22 15:41:45 -0500

    new now

--- ch9/ex2/roman.rb
+++ ch9/ex2/roman.rb
@@ -18,4 +18,29 @@ class Roman
 		end
 		s
 	end
+	def new_convert(i)
+		sym = {
+			:M => 1000,
+			:CM => 900,
+			:D => 500,
+			:CD => 400,
+			:C => 100,
+			:XC => 90,
+			:L => 50,
+			:XL => 40,
+			:X => 10,
+			:IX => 9,
+			:V => 5,
+			:IV => 4,
+			:I => 1
+		}
+		s = ""
+		t = i
+		sym.each do |k, v|
+			d = t / v
+			t = t % v
+			s += k.to_s * d
+		end
+		s
+	end
 end
--- ch9/ex2/roman_test.rb
+++ ch9/ex2/roman_test.rb
@@ -6,11 +6,17 @@ class TestRoman < MiniTest::Unit::TestCase
 	@roman = Roman.new
   end
   def test
+  	#old
   	assert_equal "VIIII", @roman.convert(9)
   	assert_equal "MXII", @roman.convert(1012)
   	assert_equal "CI", @roman.convert(101)
   	assert_equal "X", @roman.convert(10)
   	assert_equal "I", @roman.convert(1)
   	assert_equal "DX", @roman.convert(510)
+  	#new
+  	assert_equal "IV", @roman.new_convert(4)
+  	assert_equal "MCMLXXXIV", @roman.new_convert(1984)
+  	assert_equal "CCXXVI", @roman.new_convert(226)
+  	assert_equal "CMXCVIII", @roman.new_convert(998)
   end
 end
